819b0be5c273cad9cef92017a672908bcd40cf4f
koalacodee <koalacodee@gmail.com>
2025-08-26 19:44:14 +0300

    Update index.ts to use env vars

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -32,7 +32,7 @@ import {
 import { Entity, UpdateEmployeeDto } from "./employees";
 import { CreateDriverDto } from "./drivers";
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
 
